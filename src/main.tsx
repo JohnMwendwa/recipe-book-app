26be6b4049365ec0ec3bc5bc33ffb56c39b0cd0e
unknown <72663882+JohnMwendwa@users.noreply.github.com>
2024-10-28 16:33:13 +0300

    Turn project to use typescript

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "@fontsource/roboto/700.css";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
